7cee2aa37ca4b760c1d68e5d869579b8b9fe8007
Rafael De Luca <deluca1712@gmail.com>
2020-02-20 12:30:29 -0300

    Laço for e while

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,23 +13,51 @@ public class Program {
 
 		System.out.print("Informe um nome: ");
 		String name = sc.nextLine();
-
+		System.out.print("Informe uma idade: ");
+		int idade = sc.nextInt();
 		System.out.println("O nome informado foi " + name + ".");
+		System.out.println("A idade informada foi " + idade + " anos.");
 
-		Thread.sleep(3000);
+		// vetor de números
+		int vetor[] = { 80, 200, 250, 400, 800, 950 };
 
-		System.out.println("Primeiros 20 números inteiros, ordem crescente:");
+		// matriz de strings
+		String names[][] = { { "Mr. ", "Mra. ", "Mrs. ", "Teacher. " }, { "João", "Juliana", "Pereiras", "Roberto" },
+				{ " da Silva", " de Luca", " Albuquerque", " da Costa" }
 
-		for (int i = 0; i <= 20; i++) {
-			System.out.println(i);
+		};
+
+		// impressão do vetor
+		int i = 0, j = 0;
+		for (i = 0; i < vetor.length; i++) {
+			System.out.println(vetor[i]);
+		}
+		System.out.println("");
+
+		// impressão da matriz
+		for (i = 0; i < names.length; i++) {
+			for (j = 0; j < names[0].length; j++) {
+				if (i < names.length - 2)
+					System.out.println("O Nome na posição #" + i + j + " é: " + names[0][j] + names[1][j] + names[2][j]);
+				// System.out.println("O Nome na posição #" + i + j + " é: " + names[i][j]);
+
+			}
+
 		}
 
+		// pausa
 		Thread.sleep(3000);
 
-		System.out.println("Primeiros 30 números inteiros, ordem decrescente:");
-		int j = 30;
-		while (j > 0) {
-			System.out.println(j);
+		System.out.println("\nPrimeiros 20 números inteiros, ordem crescente:");
+
+		for (i = 0; i <= 20; i++) {
+			System.out.print(i+" ");
+		}
+
+		System.out.println("\nPrimeiros 30 números inteiros, ordem decrescente:");
+		j = 30;
+		while (j >= 0) {
+			System.out.print(j+" ");
 			j--;
 
 		}
